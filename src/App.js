8ca3f140ceaae5303de2a892dc1932868ce1786e
kaoru.shiraga <karats24.kaoru@gmail.com>
2023-09-30 13:00:51 +0900

    HeaderButton押下時のCartComponentモーダル表示機能の追加

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  // CartとModalの状態管理用
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  }
   return (
-    <>
-      <Header />
+    <CartProvider>
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    // Contextからカートの合計個数を取得する
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0)
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>カート</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
