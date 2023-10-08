d19826e8d421f2b74799a05fb881b8339d7e1418
kaoru.shiraga <karats24.kaoru@gmail.com>
2023-10-08 11:33:22 +0900

    カート内でのアイテム追加・削除処理の追加、カートボタンアニメーションの追加

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
+    const { items } = cartCtx;
+
     // Contextからカートの合計個数を取得する
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0)
+
+    // カートボタンにアニメーションを追加する
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        // タイマー経過後、設定したアニメーションを削除する
+        // アニメーションbumpと同様のタイムを設定する
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300)
+
+        // 古いタイマーをクリアする
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items])
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,31 +2,90 @@ import { useReducer } from "react";
 import CartContext from "./cart-context";
 
 const defaultCartState = {
-    items: [],
-    totalAmount: 0
-}
+  items: [],
+  totalAmount: 0,
+};
 
 const cartReducer = (state, action) => {
-    if (action.type === 'ADD') {
-        const updateItems = state.items.concat(action.item);
-        const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+  if (action.type === "ADD") {
+    // カート内に同様のアイテムがあるかをチェックする
+    // 存在する場合、そのアイテムのindexを格納する
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
 
-        return {
-            items: updateItems,
-            totalAmount: updateTotalAmount
-        };
+    // 上記で取得したindexのアイテムを取得する
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    // カード内に同様のアイテムが存在した場合
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+      // カート内に同様のアイテムが存在しなかった場合
+    } else {
+      updatedItems = state.items.concat(action.item);
     }
-    return defaultCartState;
-}
+
+    const updateTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
+    };
+  } else if (action.type === "REMOVE") {
+    // 削除対象アイテムのindexを取得する
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    // 上記で取得したindexのアイテムを取得する
+    const existingCartItem = state.items[existingCartItemIndex];
+    // カート内の合計金額の再計算を行う
+    const updateTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id)
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+      
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updateTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
   const addItemToCartHandler = (item) => {
-      dispatchCartAction({type: 'ADD', item: item})
+    dispatchCartAction({ type: "ADD", item: item });
   };
 
-  const removeItemFromCartHandler = (id) => {};
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
     items: cartState.items,
@@ -34,7 +93,11 @@ const CartProvider = (props) => {
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
